aeaf7177f64772e9f6e538b3885ed364451bc7de
4rg0nsw <erick.argo@gmail.com>
2020-09-09 02:06:20 -0300

    Api consulta Alunos/Cursos

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,8 +12,18 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+*/
+
+Route::post('auth/login', 'Api\AuthController@login');
+
+
+Route::group(['middleware' => 'apiJwt'], function(){
+    #http://cursos-jobim.test/api/alunosApi
+    Route::get('alunosApi', 'Api\\UserController@index');
+
+    #http://cursos-jobim.test/api/alunosApi
+    Route::get('cursoApi', 'Api\CursoController@index');
 });
